74813cc21afd2ce89331d249168b6c4510cf7f0c
Vishal Patel <85049843+venteury@users.noreply.github.com>
2023-11-21 17:00:07 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,15 +26,16 @@ const About = () => {
         </AboutImageContainer>
         <AboutDetailsContainer>
           <StyledParagraph>
-            My name is Vishal Patel, currently pursuing Bachelor of Technology
-            in University Institute of Engineering and Technology, Kanpur. I am
-            a passionate fullstack developer.
+               My name is Vishal Patel, currently working as fullstack developer at
+            AICAN Pvt Ltd and completed my Bachelor of Technology degree from
+            University Institute of Engineering and Technology, Kanpur. I am a
+            passionate fullstack developer.
           </StyledParagraph>
           <StyledParagraph>
-            I love to design and create web apps from scratch. The technologies,
+                  I love to design and create web apps from scratch. The technologies,
             tools and languages I am using to build my projects are HTML, CSS,
             JavaScript, REACT JS, Bootstrap, Express.js,MongoDB, Git and GitHub,
-            and VS Code.
+            Framer Motion, Chart JS, Tailwind and VS Code.
           </StyledParagraph>
           <StyledParagraph>
             I have eagerness to learn something new every day and I love to
@@ -43,7 +44,7 @@ const About = () => {
           </StyledParagraph>
           <Resume>
             <ResumeLink
-              href="https://drive.google.com/file/d/1JeGEz7WvWoezuF-0gU3mjtVCXpHq7Z6e/view?usp=share_link"
+              href="https://drive.google.com/file/d/1v6CDGprfWhc4MMm3poNkMQvKBug9qHUp/view?usp=sharing"
               target="_blank"
             >
               Download Resume
